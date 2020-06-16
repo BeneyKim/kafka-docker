35b70a0bcaaf9322d16865b2bc02a391c7dbaec2
김병훈/책임연구원/IoT솔루션Task(beney.kim@lge.com) <beney.kim@lge.com>
2020-06-16 16:27:15 +0900

    Add Test Scenario

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
+
+# Window
+docker run --rm -v //var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
+
+# Linux
+#docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
